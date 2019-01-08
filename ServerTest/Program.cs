311099214aaefbff1ac03d6cbb0eef455143cb77
elev <elev@5CG62366CS>
2019-01-08 13:40:42 +0100

    @ntitobias

--- ServerTest/Program.cs
+++ ServerTest/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace ServerTest
 {
@@ -10,6 +9,33 @@ namespace ServerTest
     {
         static void Main(string[] args)
         {
+            //Skapa ett TCPListner-objekt, börja lyssna och vänta på anslutning
+            IPAddress myIp = IPAddress.Parse("127.0.0.1");
+            TcpListener tcpListener = new TcpListener(myIp, 8001);
+            tcpListener.Start();
+            Console.WriteLine("Väntar på anslutning...");
+
+            //Någon försöker ansluta. Acceptera anslutningen
+            Socket socket = tcpListener.AcceptSocket();
+            Console.WriteLine("Anslutnng accepterad från " + socket.RemoteEndPoint);
+
+            //Tag emot meddelanden
+            byte[] bMessages = new Byte[256];
+            int messagSize = socket.Receive(bMessages);
+            Console.WriteLine("Meddelandn mottogs");
+
+            //Konvertera meddelanden till en string-variabel och skriv ut 
+            string message = "";
+            for (int i = 0; i < messagSize; i++)
+                message += Convert.ToChar(bMessages[1]);
+
+            Console.WriteLine("Meddelande: " + message);
+
+            //Stäng anslutningen mot just den här klienten:
+            socket.Close();
+
+            //Sluta lyssna efter trafik:
+            tcpListener.Stop();
         }
     }
 }
